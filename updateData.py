9f3cef9a223455dac211f14403cf49e4997fa454
icy40141 <60399768+icy40141@users.noreply.github.com>
2020-11-09 14:45:49 -0500

    Update updateData.py

--- updateData.py
+++ updateData.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 urlCovidSet = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
 urlCovPrison = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSAMWHkg82Nv2abt5CXl7Z_6zfWJY5KZXjo3dnwA83DlkQbKeYKVcePcYqQ7F0F_TkYsGOhhbe3BDGw/pub?gid=19995249&single=true&output=csv'
 def updateDB():
+    print("updating")
     dictCovid=[]
     dictPrison = []
     r = requests.get(urlCovidSet, allow_redirects=True)
@@ -14,7 +15,7 @@ def updateDB():
     count = 0
     covid = open('counties_covid_data.csv')
     jail = open('jail_prison_data.csv', encoding="utf8")
-    client = MongoClient('mongodb://Richard:xqqMyBFF51KlZgxx@cluster0-shard-00-00.jy5gc.mongodb.net:27017,cluster0-shard-00-01.jy5gc.mongodb.net:27017,cluster0-shard-00-02.jy5gc.mongodb.net:27017/<covid>?ssl=true&replicaSet=atlas-4e1z3x-shard-0&authSource=admin&retryWrites=true&w=majority')
+    client = MongoClient()
     db = client["Project_447"]
     collection_covid = db["covid_counties"]
     collection_jail = db["covid_jail"]
@@ -23,9 +24,8 @@ def updateDB():
     collection_covid.delete_many({})
     collection_jail.delete_many({})
     for line in covidLines:
-        print(count)
         line = line.split(",")
-        post = {"_id":count,"date": line[0], "county": line[1], "state": line[2], "cases": int(line[4]),
+        post = {"_id":count,"date": line[0], "county": line[1], "state": line[2],"FIPS":line[3], "cases": int(line[4]),
                 "deaths": int(line[5])}
         count += 1
         dictCovid.append(post)
@@ -61,3 +61,4 @@ schedule.every().day.at("23:59").do(updateDB)
 while 1:
     schedule.run_pending()
     time.sleep(60)
+    print("Waiting")
